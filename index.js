cee46ac450dd4a93d3e472137147c2854e023bf2
mynulislam75 <mynulislam181120@gmail.com>
2022-07-02 13:18:07 +0600

    added new

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 console.log(1234)
 
 // Getting email id
-const emailId=document.getElementById('email-id');
+const emailId = document.getElementById('email-id');
 // Getting password id
-const passwordId=document.getElementById('password-id');
+const passwordId = document.getElementById('password-id');
 // Get button id
-const buttonId=document.getElementById('button-id')
+const buttonId = document.getElementById('button-id')
 
 // Addeventlistner on button
-buttonId.addEventListener('click',function(){
-    console.log(999)
-    emailValue=emailId.value;
-    passwordValue=passwordId.value;
-   
-    if(emailValue=='abcd@gmail.com' && passwordValue=='1234abcd'){
-      window.location.href='banking.html';
-      console.log('okkk')
-    }
+buttonId.addEventListener('click', function () {
+  console.log(999)
+  emailValue = emailId.value;
+  passwordValue = passwordId.value;
 
-    // clear email and password field
-    emailId.value='';
-    passwordId.value='';
+  if (emailValue == 'abcd@gmail.com' && passwordValue == '1234abcd') {
+    window.location.href = 'banking.html';
+    console.log('okkk')
+  }
+  else if (emailValue !== 'abcd@gmail.com' || passwordValue !== '1234abcd') {
+    alert('Please enter right email and password');
+    emailId.value = '';
+    passwordId.value = '';
+    return;
+  }
+
+  // clear email and password field
+  emailId.value = '';
+  passwordId.value = '';
 })
